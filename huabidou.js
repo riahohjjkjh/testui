f792b78386ef1ee452a27e794cb65939c006af96
riahohjjkjh <142058674+riahohjjkjh@users.noreply.github.com>
2023-08-12 00:24:33 +0200

    Create huabidou.js
    tuhjkiufdffdfdf

---
+++ huabidou.js
@@ -0,0 +1,140 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+			function audioPlayerSetupDone(){
+				/* This will get called when component is ready to receive public function calls. */
+				//console.log('audioPlayerSetupDone');
+			}
+			
+			// SETTINGS
+			var ap_settings = {
+				/* activePlaylist: set active playlist that will be loaded on beginning (pass element 'id' attributte) */
+				activePlaylist: 'playlist1',
+				soundcloudApiKey: '',
+
+				/*defaultVolume: 0-1 (Irrelevant on ios mobile) */
+				defaultVolume:0.5,
+				/*autoPlay: true/false (false on mobile by default, important!) */
+				autoPlay:true,
+				/*randomPlay: true/false */
+				randomPlay:false,
+				/*loopingOn: true/false (loop on the end of the playlist) */
+				loopingOn:true,
+				
+				/* addNumbersInPlaylist: true/false. Prepend numbers in playlist items. */
+				addNumbersInPlaylist: false,
+				
+				/* useSongNameScroll: true/false. Use song name scrolling. */
+				useSongNameScroll: true,
+				/* scrollSpeed: speed of the scroll (number higher than zero). */
+				scrollSpeed: 1,
+				/* scrollSeparator: String to append between scrolling song name. */
+				scrollSeparator: '&nbsp;&#42;&#42;&#42;&nbsp;',
+				
+				/* mediaTimeSeparator: String between current and total song time. */
+				mediaTimeSeparator: '&nbsp;-&nbsp;',
+				/* seekTooltipSeparator: String between current and total song position, for progress tooltip. */
+				seekTooltipSeparator: '&nbsp;/&nbsp;',
+				
+				/* useRollovers: use button rollovers, true/false. */
+				useRollovers:false,
+				/* buttonsUrl: url of the buttons for normal and rollover state, so I dont hardcode them in jquery (rollover state is optional). */
+				buttonsUrl: {prev: 'img/prev.png', prevOn: 'img/prev_on.png', 
+						  	 next: 'img/next.png', nextOn: 'img/next_on.png', 
+						 	 pause: 'img/pause.png', pauseOn: 'img/pause_on.png',
+						 	 play: 'img/play.png', playOn: 'img/play_on.png',
+						 	 volume: 'img/volume.png', volumeOn: 'img/volume_on.png', 
+							 mute: 'img/mute.png', muteOn: 'img/mute_on.png', 
+						  	 loop: 'img/loop.png', loopOn: 'img/loop_on.png',
+						  	 shuffle: 'img/shuffle.png', shuffleOn: 'img/shuffle_on.png'}
+			};
+			
+			soundManager.allowScriptAccess = 'always';
+			soundManager.debugMode = false;
+			soundManager.noSWFCache = true;
+			soundManager.useConsole = false;
+			soundManager.waitForWindowLoad = true;
+			soundManager.url = 'swf/';//path to flash files
+			soundManager.flashVersion = 9;
+			soundManager.preferFlash = false; 
+			soundManager.useHTML5Audio = true;
+			
+
+			var audio = document.createElement('audio'), mp3Support, oggSupport;
+			if (audio.canPlayType) {
+			   mp3Support = !!audio.canPlayType && "" != audio.canPlayType('audio/mpeg');
+			   oggSupport = !!audio.canPlayType && "" != audio.canPlayType('audio/ogg; codecs="vorbis"');
+			}else{
+				//for IE<9
+				mp3Support = true;
+				oggSupport = false;
+			}
+			//console.log('mp3Support = ', mp3Support, ' , oggSupport = ', oggSupport);
+			
+			/*
+			FF - false, true
+			OP - false, true
+			
+			IE9 - true, false 
+			SF - true, false 
+			
+			CH - true, true
+			*/
+		
+		    soundManager.audioFormats = {
+			  'mp3': {
+				'type': ['audio/mpeg; codecs="mp3"', 'audio/mpeg', 'audio/mp3', 'audio/MPA', 'audio/mpa-robust'],
+				'required': mp3Support
+			  },
+			  'mp4': {
+				'related': ['aac','m4a'],
+				'type': ['audio/mp4; codecs="mp4a.40.2"', 'audio/aac', 'audio/x-m4a', 'audio/MP4A-LATM', 'audio/mpeg4-generic'],
+				'required': false
+			  },
+			  'ogg': {
+				'type': ['audio/ogg; codecs=vorbis'],
+				'required': oggSupport
+			  },
+			  'wav': {
+				'type': ['audio/wav; codecs="1"', 'audio/wav', 'audio/wave', 'audio/x-wav'],
+				'required': false
+			  }
+			};
+
+			
+			jQuery(document).ready(function() {
+				var $ = jQuery.noConflict();
+			    $.html5audio('#componentWrapper', ap_settings, 'sound_id1');
+			    ap_settings = null;
+    	    });
+
+}
+/*
+     FILE ARCHIVED ON 10:38:55 Apr 08, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:20:44 Aug 11, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 102.823
+  exclusion.robots: 0.145
+  exclusion.robots.policy: 0.133
+  cdx.remote: 0.064
+  esindex: 0.01
+  LoadShardBlock: 71.161 (3)
+  PetaboxLoader3.datanode: 84.189 (5)
+  load_resource: 433.663 (2)
+  PetaboxLoader3.resolve: 381.069 (2)
+*/
